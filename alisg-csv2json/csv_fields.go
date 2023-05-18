ebf35767684514354972518c8b88783d03ed7cad
imeks100 <25627588+imeks100@users.noreply.github.com>
2023-05-18 15:46:33 +0800

    Add files via upload

---
+++ alisg-csv2json/csv_fields.go
@@ -0,0 +1,13 @@
+package main
+
+// csv ordered filed name, starts with 0
+const (
+	NIC_TYPE = iota
+	SECURITY_GROUP_RULE_TYPE
+	POLICY
+	IP_PROTOCOL
+	PORT_RANGE
+	PRIORITY
+	AUTHORIZATION_OBJECT
+	DESCRIPTION
+)
