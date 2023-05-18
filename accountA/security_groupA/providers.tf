78d7bc99ed5253033d9e6ea7d43a7bb452e9ddfa
imeks100 <25627588+imeks100@users.noreply.github.com>
2023-05-18 14:42:27 +0800

    Update providers.tf

--- accountA/security_groupA/providers.tf
+++ accountA/security_groupA/providers.tf
@@ -1,3 +1,14 @@
 provider "alicloud" {
   region = var.region
 }
+
+terraform {
+ backend "oss" {
+   bucket              = "demo-bucket-shh"
+   prefix              = "azure-pipelines/csv2json"
+   key                 = "tfstate"
+   region              = "cn-shanghai"
+   tablestore_endpoint = "https://tfstate.cn-shanghai.ots.aliyuncs.com"
+   tablestore_table    = "statelock"
+ }
+}
